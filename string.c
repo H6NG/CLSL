9b19256132bc4f86a848db05d6553edafe8119d0
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2025-07-27 12:07:32 -0400

    sizeof() char letter words and %zu

--- string.c
+++ string.c
@@ -8,7 +8,28 @@ int main(){
     char television[] = "you're watching the TV"; 
     printf("%s\n", television); 
 
+    television[0] = 'a'; // char is always with '' not "" 
+
     printf("%c\n", television[0]); 
+
+
+    char car[] = "Volvo";
+
+    int countletter = sizeof(car) / sizeof(car[0]); //you want to know how many bytes per letter smartass
+    
+    for(int i = 0; i<countletter; i++){
+        printf("%c ", car[i]);
+        if(i==(countletter-1)){
+            printf("\n"); 
+            break;
+        }
+    }
+
+    char greetings[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+
+    printf("%s\n", greetings); // it puts the letters together ç
+
+    printf("%zu\n", sizeof(greetings)); // don't forget %zu for sizeof() 
 
 
 
